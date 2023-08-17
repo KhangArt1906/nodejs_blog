ddec5bda81772af1ac7545aee30d0aa38f06a824
Nguyen Phuoc Duy Khang <duykhangart@gmail.com>
2023-08-17 16:25:35 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(__dirname + "/public"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 //http logger
 app.use(morgan("combined"));
@@ -21,22 +29,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//Home, search, contact
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.query.q);
-  res.render("search");
-});
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
